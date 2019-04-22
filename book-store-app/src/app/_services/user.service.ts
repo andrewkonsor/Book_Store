8865beb09d6c097352f5c56a60b655400ce4cb55
andrewkonsor <38930952+andrewkonsor@users.noreply.github.com>
2019-04-22 16:48:01 -0500

    Update user.service.ts

--- book-store-app/src/app/_services/user.service.ts
+++ book-store-app/src/app/_services/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { environment } from 'src/environments';
+import { environment } from '../environments/environment';
 import { User } from 'src/app/_models';
 
 @Injectable({ providedIn: 'root' })
